567c0ab89694686e2cac71f254c5edbf8fb626ce
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2023-09-04 14:46:36 +0200

    update jacketsHer
    fixed link on images

--- Js/jacketsHer.js
+++ Js/jacketsHer.js
@@ -28,12 +28,13 @@ async function getJackets() {
         cssClass = "fa";
      }
    
-        jacketsContainer.innerHTML += `<a href="Info.html?id=${jacket.id}" class="jackets-Shop">
-        <img  src="${jacket.image}" alt="${jacket.description}" />
-        <h2 >${jacket.title}</h2>
-        <p class="Price"> ${jacket.price} </p>
-        <i class="${cssClass} fa-heart" data-id="${jacket.id}"  data-image="${jacket.image}" data-price="${jacket.price}" data-description="${jacket.description}"  ></i>
-        </a>` }); 
+     jacketsContainer.innerHTML += `<div class="jackets-Shop">
+     <a href="Info.html?id=${jacket.id}">
+     <img  src="${jacket.image}" alt="${jacket.description}" />
+     <h2 >${jacket.title}</h2></a>
+     <p class="Price"> ${jacket.price} </p>
+     <i class="${cssClass} fa-heart" data-id="${jacket.id}" data-title="${jacket.title}" data-image="${jacket.image}" data-price="${jacket.price}" data-description="${jacket.description}"  ></i>
+     </div>` });  
     
    
     const favButtons=document.querySelectorAll(".jackets-Shop i");
