7c90c456137e668b19a61f37d19964c8f926a7bf
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2023-09-07 15:50:08 +0200

    added jackets her js  text
    added js her text and fixed some css (hearts and contact osv)

--- Js/jacketsHer.js
+++ Js/jacketsHer.js
@@ -1,32 +1,26 @@
-import { getJackets} from "./jacketsList.js";
-const jacketsContainer = document.querySelector(".jackets-Shop");
-console.log(getJackets)
+import { getJackets, createHTML} from "./jacketsList.js";
+import { toggleFavorite } from "./utils/favFunctions.js";
 
-// if (jacket.favorite === true){getJackets()}
 
-async function displayJackets(){
-    const jacketList = await getJackets();
+async function displayJackets(){ 
+    try{
+      
     const jacketsContainer = document.querySelector(".jackets-Shop");
+    const jacketList = await getJackets();
+    
+    jacketList.forEach((jacket)=>{
+      if (jacket.gender === "Female"){
+        createHTML (jacket, jacketsContainer);
+
+        
+      }
+    });  
+        
+}  catch(error){
+  console.log(error)
+}
+}
 
 
-   jacketList.forEach((jacket) =>{
-    let cssClass="far";})
-
-jacketsContainer.innerHTML += `<div class="jackets-container">
-<a href="Info.html?id=${jacket.id}">
-<img  src="${jacket.image}" alt="${jacket.description}" />
-<h2 >${jacket.title}</h2></a>
-<p class="Price"> ${jacket.price} </p>
-<i class="${cssClass} fa-heart" data-id="${jacket.id}" data-title="${jacket.title}" data-image="${jacket.image}" data-price="${jacket.price}" data-description="${jacket.description}"  ></i>
-</div>`};
-
-// jacket = $.sort ((a,b)=>{
-//     if (a.id< b.id){
-//         console.log("hhoogseoisnoi")
-//     }
-// })
-
-
- console.log(displayJackets)
 displayJackets()
-//test things
+     
--- Js/jacketsHim.js
+++ Js/jacketsHim.js
@@ -24,15 +24,3 @@ async function displayJackets(){
 
 displayJackets()
      
-  //  function createHTML(jacket){
-    
-  //   jacketsContainer.innerHTML += `<div class="jackets-Shop">
-  //   <a href="Info.html?id=${jacket.id}" class"Jacket_info">
-  //   <img  src="${jacket.image}" alt="${jacket.description}" />
-  //   <h2 >${jacket.title}</h2></a>
-  //   <i class="far fa-heart" data-id="${jacket.id}" data-title="${jacket.title}" data-image="${jacket.image}" data-price="${jacket.price}" data-description="${jacket.description}"  ></i>
-  //   <p class="Price"> ${jacket.price} </p>
-  //   </div>` };
-
-// for ( let i=0; i<jacketList.length; i++) {
-//     const jacket = jacket[i];
