d5763a9f5d67b3e55f04889ec6e28a6d25f29acb
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2023-09-30 19:13:03 +0200

    sale price and total price updated
    sale price now shows in cart, and total updates on how many items are in the cart

--- Js/cart.js
+++ Js/cart.js
@@ -37,56 +37,70 @@ document.addEventListener("DOMContentLoaded", async () => {
           const cartItemDiv = document.createElement("div");
           cartItemDiv.classList.add("Cart-Bergolos");
 
+          const imgLink = document.createElement("a");
+          imgLink.href= `/info.html?id=${jacket.id}`;
+          // imgLink.target ="_blank";
+
           const img = document.createElement("img");
           img.src = jacket.image;
           img.alt = jacket.description;
 
-          const titleDiv = document.createElement("div");
+          imgLink.appendChild(img);
+
+
           const priceDiv = document.createElement("div");
           const amountDiv = document.createElement("div");
           const totalDiv = document.createElement("div");
           const colorDiv = document.createElement("div");
+          const titleAndTagsDiv = document.createElement("div");
 
 
-          const titleH3 = document.createElement("h3");
+          const titleH3 = document.createElement("h4");
+          const tagsH3 = document.createElement("h4");
           const priceP = document.createElement("p");
-          if (jacket.onSale === true) {
-            priceP.textContent = `$${jacket.discountedPrice}`;
-          } else {
-            priceP.textContent = `$${jacket.price}`;
-          }
           const amountH4 = document.createElement("h4");
           const totalP = document.createElement("p");
           const color = document.createElement("h4");
           const sizeP = document.createElement ("p");
-         
 
-          titleH3.textContent = jacket.title + `` + jacket.tags;
-          priceP.textContent = `$${jacket.price}`;
+
+          titleH3.textContent = jacket.title;
+          tagsH3.textContent = jacket.tags;
+          if (jacket.onSale) {
+            priceP.textContent = `$${jacket.discountedPrice}`;
+            cartItem.totalPrice = parseFloat(jacket.discountedPrice) * cartItem.quantity;
+          }else{
+            priceP.textContent = `$${jacket.price}`;
+            cartItem.totalPrice = parseFloat(jacket.price)* cartItem.quantity;
+          }
           amountH4.classList.add("amount");
           amountH4.textContent = cartItem.quantity;
           color.textContent = jacket.baseColor;
           sizeP.textContent = `size: ${cartItem.size}`;
 
-          titleDiv.appendChild(titleH3);
+
           priceDiv.appendChild(priceP);
           amountDiv.appendChild(amountH4);
           colorDiv.appendChild(color);
+          titleAndTagsDiv.appendChild(titleH3);
+          titleAndTagsDiv.appendChild(tagsH3)
 
-          const totalPrice = parseFloat(jacket.price) * 1;
-          totalP.textContent = `$${totalPrice.toFixed(2)}`;
+          totalP.textContent = `$${cartItem.totalPrice.toFixed(2)}`;
 
           totalDiv.appendChild(totalP);
 
-          cartItemDiv.appendChild(img);
-          cartItemDiv.appendChild(titleDiv);
+          cartItemDiv.appendChild(imgLink);
+          cartItemDiv.appendChild(titleAndTagsDiv);
           cartItemDiv.appendChild(priceDiv);
           cartItemDiv.appendChild(amountDiv);
           cartItemDiv.appendChild(colorDiv);
           cartItemDiv.appendChild(totalDiv);
 
           cartContainer.appendChild(cartItemDiv);
+
           
+
+
         }else{
           const errorMessage = "jacket not found in the cart";
           const messageType = "Error";
