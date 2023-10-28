14bd4276e26f8cc8942f4e3be60fa8e7b6e8ede1
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2023-10-28 13:17:24 +0200

    info buttons updated
    Made the sizes, remove, added and go to cart work

--- Js/info.js
+++ Js/info.js
@@ -3,45 +3,43 @@ import { displayMessage } from "./utils/errorMessage.js";
 import { NavbarClosing } from "./utils/hamburgerMenu.js";
 import { fetchJackets } from "./jacketsList.js";
 
-let cartItem;
-let quantity = parseInt (localStorage.getItem ("quantity")) || 0;
+
+let quantity = parseInt(localStorage.getItem("quantity")) || 0;
 let selectedSize = "";
 let sizeSelected = false;
 let currentTextIndex = 0;
-
-
+let jacket;
 
 document.addEventListener("DOMContentLoaded", async () => {
   NavbarClosing();
 });
-
 
 const jacketContainer = document.querySelector(".jacket-container");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-document.addEventListener("DOMContentLoaded", async()=>{
-  try{
-  const jacketData = await fetchJackets();
-  const jacket = jacketData.find((jacket)=> jacket.id === parseInt(id,10));
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const jacketData = await fetchJackets();
+    jacket = jacketData.find((jacket) => jacket.id === parseInt(id, 10));
 
-  if (jacket){
-    document.title = `${jacket.name}`;
-    createHTML(jacket);
+    if (jacket) {
+      document.title = `${jacket.name}`;
+      const cart = getCartFromLocalStorage();
+      const cartItem = cart.find((item) => item.id === jacket.id && item.size === selectedSize);
 
-  
-    
-  }else{
-    if(window.location.pathname.includes("info.html")){
-    const errorMessage ="jacket not found";
-    const messageType ="Error";
-    displayMessage(messageType, errorMessage, ".jacket-container");
+      createHTML(jacket, cartItem);
+    } else {
+      if (window.location.pathname.includes("info.html")) {
+        const errorMessage = "Jacket not found";
+        const messageType = "Error";
+        displayMessage(messageType, errorMessage, ".jacket-container");
+      }
+    }
+  } catch (error) {
+    console.error(error);
   }
-}
-}catch (error){
-  console.error(error);
-}
 });
 
 const buttonTexts = ["Add to cart", "Added!"];
@@ -51,78 +49,77 @@ if (storedButtonText && buttonTexts.includes(storedButtonText)) {
   currentTextIndex = buttonTexts.indexOf(storedButtonText);
 }
 
+export function createHTML(info, cartItem) {
+  console.log("Creating HTML for jacket with info:", info);
+  const image = info.images[0].src;
+  const altText = info.images[0].alt;
+  const price = (parseInt(info.prices.regular_price, 10) / 100).toLocaleString('nb-NO', {
+    style: "currency",
+    currency: "NOK",
+  });
+  const discountedPrice = (parseInt(info.prices.sale_price, 10) / 100).toLocaleString('nb-NO', {
+    style: "currency",
+    currency: "NOK",
+  });
 
-export function createHTML(info) {
-const image = info.images[0].src 
-const altText= info.images[0].alt
-  const price= (parseInt(info.prices.regular_price, 10) /100).toLocaleString( 'nb-NO',{
-  style:`currency`,
-  currency: `NOK`
-
- });
- const discountedPrice= (parseInt(info.prices.sale_price, 10) /100).toLocaleString( 'nb-NO',{
-  style:`currency`,
-  currency: `NOK`
- });
-  
   jacketContainer.innerHTML += `
-                              <div class="Jacket_info ">
-                               <div >
-                                 <h1 >${info.name}</h1>
-                                  <h2>  ${info.categories[0].name}</h2>
-                                    <h3>${info.description}</h3>
-                                    
-                                    ${
-                                      info.on_sale
-                                        ? ` <p class="info_Price_original"> ${price}  </p>
-                                      <p class = "info_onSale_price">  ${discountedPrice}</p>`
-                                        : `<p class= "info_Price "> ${price}</p>`
-                                    }
-                                     <div class="Size-button"></div>
-                                      <button class="Continue_button addedToCart"> Add to cart </button>
-                                      <button class="cart_button"> </button>
-                                      <button class="info-button-remove" style="display: none;"><i class="far fa-trash-can"></i></button> 
-                                                                         </div >
-                                         <img  src="${image}" alt="${ altText}" />
-                                         </div>`;
+    <div class="Jacket_info ">
+      <div >
+        <h1 >${info.name}</h1>
+        <h2>  ${info.categories[0].name}</h2>
+        <h3>${info.description}</h3>
+        ${
+          info.on_sale
+            ? ` <p class="info_Price_original"> ${price}  </p>
+               <p class = "info_onSale_price">  ${discountedPrice}</p>`
+            : `<p class= "info_Price "> ${price}</p>`
+        }
+        <div class="Size-button"></div>
+        <button class="Continue_button addedToCart"> Add to cart </button>
+        <button class="cart_button"> </button>
+        <button class="info-button-remove" style="display: none;"><i class="far fa-trash-can"></i></button> 
+      </div >
+      <img  src="${image}" alt="${altText}" />
+    </div>`;
 
   const sizeButtonsContainer = jacketContainer.querySelector(".Size-button");
   let selectedSizeButton = null;
 
   if (info.attributes) {
-    const sizeAttribute = info.attributes.find((attr)=> attr.name === "size");
+    const sizeAttribute = info.attributes.find((attr) => attr.name === "size");
 
-    if(sizeAttribute && sizeAttribute.terms){
-      const sizes = sizeAttribute.terms.map (term=> term.name);
-      sizeAttribute.terms.forEach((term)=>{
-        
-      const sizeButton = document.createElement("Button");
-      sizeButton.textContent = term.name;
-      sizeButton.className = "Size-button";
+    if (sizeAttribute && sizeAttribute.terms) {
+      const sizes = sizeAttribute.terms.map((term) => term.name);
+      sizeAttribute.terms.forEach((term) => {
+        const sizeButton = document.createElement("button");
+        sizeButton.textContent = term.name;
+        sizeButton.className = "Size-button";
 
-      sizeButton.addEventListener("click", () => {
-        if (selectedSizeButton) {
-          selectedSizeButton.classList.remove("clicked");
-        }
-        sizeButton.classList.toggle("clicked");
-        selectedSizeButton = sizeButton;
-        selectedSize = term.name;
-        sizeSelected = true;
-        resetButtonText();
-        updateButtonText(quantity, cartItem);
-        
+        sizeButton.addEventListener("click", () => {
+          if (selectedSizeButton) {
+            selectedSizeButton.classList.remove("clicked");
+          }
+          sizeButton.classList.toggle("clicked");
+          selectedSizeButton = sizeButton;
+          selectedSize = term.name;
+          sizeSelected = true;
+          resetButtonText();
+
+          const cart = getCartFromLocalStorage();
+          const cartItem = cart.find ((item)=> item.id === jacket.id && item.size === selectedSize);
+          itemQuantity = cartItem ? cartItem.quantity : 0;
+          updateButtonText(jacket, selectedSize);
+        });
+
+        sizeButtonsContainer.appendChild(sizeButton);
       });
-  
-      sizeButtonsContainer.appendChild(sizeButton);
-    });
-  } 
-  }   
+    }
+  }
 
   const addToCartButton = jacketContainer.querySelector(".addedToCart");
   const cartButton = jacketContainer.querySelector(".cart_button");
-  const removeButton =jacketContainer.querySelector(".info-button-remove");
+  const removeButton = jacketContainer.querySelector(".info-button-remove");
   const cart = getCartFromLocalStorage();
-  const cartItem = cart.find((item)=> item.id === info.id && item.size === selectedSize);
 
   if (cartItem) {
     currentTextIndex = 1;
@@ -130,87 +127,93 @@ const altText= info.images[0].alt
     addToCartButton.textContent = `Added ${cartItem.quantity}`;
     addToCartButton.style.display = "block";
     cartButton.style.display = "block";
-    removeButton.style.display ="block"
+    removeButton.style.display = "block";
   } else {
     currentTextIndex = 0;
-    addToCartButton.style.display ="block"
+    addToCartButton.style.display = "block";
     cartButton.style.display = "none";
-    removeButton.style.display ="none"
+    removeButton.style.display = "none";
   }
-// Log additional information for debugging
-console.log("Button Text: ", cartButton);
-console.log("Price: ", price);
-console.log("On Sale: ", info.on_sale);
-console.log("categories: ", info.categories);
-console.log("cartItem", cartItem)
-console.log("cartButton:", cartButton);
-console.log("selectedSize:", selectedSize);
-console.log("Cart Contents:", cart);  
-console.log ("quantity", quantity);
-console.log("cart content", cart);
 
   addToCartButton.addEventListener("click", () => {
 
     if (sizeSelected){
        addToCart(info, selectedSize);
       currentTextIndex = 1;
-      updateButtonText (quantity, cartItem);
+      updateButtonText(jacket, selectedSize);
    
     }
   });
 
-
   cartButton.textContent = "Go to cart";
   cartButton.className = "Continue_button cart_button";
   cartButton.addEventListener("click", () => {
-    window.location.href="Checkout/Cart.html"
+    window.location.href = "Checkout/Cart.html";
   });
 
   removeButton.addEventListener("click", () => {
-    removeFromCart(id, selectedSize);
-    updateButtonText(quantity, null);
-    removeButton.style.display= "none";
-});
+    removeFromCart(info, selectedSize);
+    updateButtonText(jacket, selectedSize);
+  });
 }
-
+let itemQuantity = 0;
 export function addToCart(jacket, selectedSize) {
   const cart = getCartFromLocalStorage();
-  cartItem = cart.find((item)=> item.id === jacket.id && item.size === selectedSize);
+  let itemIndex = cart.findIndex((item) => item.id === jacket.id && item.size === selectedSize);
+  let cartItem;
+  console.log("Adding item to the cart:", jacket, selectedSize);
   
-  if (cartItem){
-    cartItem.quantity +=1;
-  }else{
-    const itemPrice = jacket.on_sale ? parseFloat (jacket.prices.sale_price) : parseFloat(jacket.prices.regular_price);
+  if (itemIndex !== -1){
+    cartItem = cart[itemIndex];
+    cartItem.quantity += 1;
+    itemQuantity = cartItem.quantity
+   cartItem.totalPrice += jacket.prices.price;
+    
+    saveCartToLocalStorage(cart);
+    updateButtonText(jacket, selectedSize);
+  } else {
+    const itemPrice = jacket.on_sale ? parseFloat(jacket.prices.sale_price) : parseFloat(jacket.prices.regular_price);
 
-  cart.push({
-    id: jacket.id,
-    name: jacket.name,
-    price: jacket.prices.price,
-    image:jacket.images[0].src, 
-    on_sale: jacket.on_sale,
-    size: selectedSize,
-    quantity: 1,
-    itemPrice: itemPrice,
-    totalPrice:itemPrice,
+    cart.push({
+      id: jacket.id,
+      name: jacket.name,
+      price: jacket.prices.price,
+      image: jacket.images[0].src,
+      on_sale: jacket.on_sale,
+      size: selectedSize,
+      quantity: 1,
+      itemPrice: itemPrice,
+      totalPrice: itemPrice,
+    });
+    itemIndex = cart.length - 1;
+    cartItem = cart[itemIndex];
+    itemQuantity = 1;
+  }
 
-  });
-  cartItem = cart[cart.length -1]
-}
-cartItem.totalPrice = cartItem.itemPrice * cartItem.quantity;
+  cartItem.totalPrice = cartItem.itemPrice * cartItem.quantity;
   saveCartToLocalStorage(cart);
-  return cartItem;
-
-  incrementQuantity(jacket, selectedSize);
-  updateButtonText(cartItem);
-}
-
-function updateButtonText() {
+  updateButtonText(jacket, selectedSize);
   const addToCartButton = jacketContainer.querySelector(".addedToCart");
   const cartButton = jacketContainer.querySelector(".cart_button");
-  const removeButton =jacketContainer.querySelector(".info-button-remove");
+  const removeButton = jacketContainer.querySelector(".info-button-remove");
+
+  addToCartButton.style.display ="block";
+  cartButton.style.display = "block";
+  removeButton.style.display = "block";
+
+  updateButtonText(jacket, selectedSize);
+}
+
+function updateButtonText(jacket, selectedSize) {
+  const cart = getCartFromLocalStorage ();
+  const cartItem = cart.find (item => item.id === jacket.id && item.size === selectedSize)
+  console.log("Updating button text with cart item:", itemQuantity);
+  const addToCartButton = jacketContainer.querySelector(".addedToCart");
+  const cartButton = jacketContainer.querySelector(".cart_button");
+  const removeButton = jacketContainer.querySelector(".info-button-remove");
 
   if (cartItem){
-    const itemQuantity = cartItem.quantity
+   
 
     addToCartButton.textContent = `Added ${itemQuantity}`;
     addToCartButton.style.display ="block";
@@ -226,7 +229,7 @@ function updateButtonText() {
 
     console.log ("itemQuantity", itemQuantity)
    
-  }else{
+  } else {
     addToCartButton.textContent = "Add to cart";
     addToCartButton.style.display="block";
     cartButton.style.display= "none";
@@ -234,43 +237,35 @@ function updateButtonText() {
   }
   localStorage.setItem("addToCartButtonText", addToCartButton.textContent);
 }
-  function resetButtonText (){
-    const addToCartButton = jacketContainer.querySelector (".addedToCart");
-    addToCartButton.textContent = "Add to cart";
-    currentTextIndex = 0;
-    localStorage.setItem ("addToCartButtonText", addToCartButton.textContent)
-  }
 
-  function removeFromCart(jacket, selectedSize){
-    const cart = getCartFromLocalStorage();
-    const itemIndex = cart.findIndex((item) => item.id === jacket.id && item.size === selectedSize);
-
-    if (itemIndex !== -1){
-      cart.splice(itemIndex, 1);
-      saveCartToLocalStorage(cart);
-    }
-updateButtonText(quantity, cartItem);
-  }
-
-
-
-function incrementQuantity(jacket,selectedSize){
-  let quantity = parseInt(localStorage.getItem("quantity")) || 0;
-  quantity += 1;
-  localStorage.setItem("quantity", JSON.stringify(quantity));
+function resetButtonText (){
+  const addToCartButton = jacketContainer.querySelector (".addedToCart");
+  addToCartButton.textContent = "Add to cart";
+  currentTextIndex = 0;
+  localStorage.setItem ("addToCartButtonText", addToCartButton.textContent)
 }
 
+function removeFromCart(jacket, selectedSize){
+  const cart = getCartFromLocalStorage();
+  let itemIndex = cart.findIndex((item) => item.id === jacket.id && item.size === selectedSize);
+
+  if (itemIndex !== -1){
+    cart.splice(itemIndex, 1);
+    saveCartToLocalStorage(cart);
+  }
+  updateButtonText(jacket, selectedSize);
+}
 
 export function saveCartToLocalStorage(cart) {
   localStorage.setItem("shoppingCart", JSON.stringify(cart));
 }
 export function getCartFromLocalStorage() {
-  try{
-  const cart = localStorage.getItem("shoppingCart");
-  return cart ? JSON.parse(cart) : [];
-}catch (error){
-  return[];
-  
+  try {
+    const cart = localStorage.getItem("shoppingCart");
+    return Array.isArray(cart) ? cart : JSON.parse (cart) || [];
+  } catch (error) {
+    return [];
+  }
 }
-}
-fetchJackets();  
+
+fetchJackets();
